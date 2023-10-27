94da8117997f0a50528a328148c9cbc022831b2c
Frederik <me@frederiknolte.com>
2023-10-27 16:47:59 +0100

    change make_mp4.py such that multiple rollouts are concatenated

--- tests/make_mp4.py
+++ tests/make_mp4.py
@@ -2,20 +2,33 @@ import os
 import numpy as np
 import imageio
 from PIL import Image
+from tqdm import tqdm
 
 
-FRAMES_PATH = 'datasets/2023-09-02'
+FRAMES_PATHS = ['PATH_TO_DATASET1',
+                'PATH_TO_DATASET2']
+SAVE_PATH = 'PATH_TO_SAVE'
 
 def make_movie(frames, path, fps=15):
+    print('Writing...')
     path += '.mp4'
     writer = imageio.get_writer(path, format="mp4", mode="I", fps=fps)
-    for frame in frames:
+    for frame in tqdm(frames):
         writer.append_data(frame)
     writer.close()
 
+frame_sets = []
+print('Loading...')
+for path in tqdm(FRAMES_PATHS):
+    frames_filenames = sorted([filename for filename in os.listdir(path) if '.PNG' in filename])
+    frames_paths = [os.path.join(path, filename) for filename in frames_filenames]
+    frame_sets.append([np.array(Image.open(filepath)) for filepath in frames_paths])
 
-frames_filenames = sorted([filename for filename in os.listdir(FRAMES_PATH) if '.PNG' in filename])
-frames_paths = [os.path.join(FRAMES_PATH, filename) for filename in frames_filenames]
-frames = [np.array(Image.open(filepath)) for filepath in frames_paths]
+# frame_sets = [s[:min(*[len(l) for l in frame_sets])] for s in frame_sets]
+max_len = max(*[len(l) for l in frame_sets])
+print('Processing...')
+for frame_set in tqdm(frame_sets):
+    frame_set.extend([np.ones_like(frame_set[0]) for _ in range(max_len - len(frame_set))])
+frames = np.concatenate(frame_sets, axis=2)
 
-make_movie(frames, os.path.join(FRAMES_PATH, 'video'))
+make_movie(frame_sets, SAVE_PATH)
