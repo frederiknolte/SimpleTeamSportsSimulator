6cddf3e78f037b55ac55a6effb894eb3152dcb4d
Frederik <me@frederiknolte.com>
2023-10-27 16:53:43 +0100

    change make_mp4.py such that frame loading is dynamic -> lower memory footprint

--- tests/make_mp4.py
+++ tests/make_mp4.py
@@ -5,30 +5,31 @@ from PIL import Image
 from tqdm import tqdm
 
 
-FRAMES_PATHS = ['PATH_TO_DATASET1',
-                'PATH_TO_DATASET2']
-SAVE_PATH = 'PATH_TO_SAVE'
+FRAMES_PATHS = ['../tests/datasets/1_new',
+                '../tests/datasets/1_old',
+                ]
+SAVE_PATH = '../tests/datasets/1.mp4'
+FPS = 50
 
-def make_movie(frames, path, fps=15):
-    print('Writing...')
-    path += '.mp4'
-    writer = imageio.get_writer(path, format="mp4", mode="I", fps=fps)
-    for frame in tqdm(frames):
-        writer.append_data(frame)
-    writer.close()
 
-frame_sets = []
-print('Loading...')
-for path in tqdm(FRAMES_PATHS):
+frames_paths = []
+for path in FRAMES_PATHS:
     frames_filenames = sorted([filename for filename in os.listdir(path) if '.PNG' in filename])
-    frames_paths = [os.path.join(path, filename) for filename in frames_filenames]
-    frame_sets.append([np.array(Image.open(filepath)) for filepath in frames_paths])
+    frames_paths.append([os.path.join(path, filename) for filename in frames_filenames])
 
-# frame_sets = [s[:min(*[len(l) for l in frame_sets])] for s in frame_sets]
-max_len = max(*[len(l) for l in frame_sets])
-print('Processing...')
-for frame_set in tqdm(frame_sets):
-    frame_set.extend([np.ones_like(frame_set[0]) for _ in range(max_len - len(frame_set))])
-frames = np.concatenate(frame_sets, axis=2)
 
-make_movie(frame_sets, SAVE_PATH)
+example_frames = [Image.open(filepaths[0]) for filepaths in frames_paths]
+max_len = max(*[len(l) for l in frames_paths])
+writer = imageio.get_writer(SAVE_PATH, format="mp4", mode="I", fps=FPS)
+for i in tqdm(range(max_len)):
+    frame = []
+    for j, dataset in enumerate(frames_paths):
+        if i >= len(dataset):
+            frame.append(np.zeros_like(example_frames[j]))
+        else:
+            frame.append(np.array(Image.open(dataset[i])))
+
+    frame = np.concatenate(frame, axis=1)
+    writer.append_data(frame)
+
+writer.close()
